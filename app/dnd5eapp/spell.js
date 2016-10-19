4b955e2d69521c027710dbbb90ea194d30491a64
Michael Irizarry <mirizarry@listrak.com>
2016-10-19 13:04:56 -0400

    cleanup of files that should have been gitignored

--- app/dnd5eapp/spell.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Spell = (function () {
-    function Spell() {
-        this.classes = [];
-    }
-    return Spell;
-}());
-exports.Spell = Spell;
-//# sourceMappingURL=spell.js.map
