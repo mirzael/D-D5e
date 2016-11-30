4d65036f5abda9672bc5a27e75ef45c7662d9536
Michael Irizarry <mirizarry@listrak.com>
2016-11-30 08:31:39 -0500

    Added button on navbar to reach other pages on mobile devices

--- app/dnd5eapp/dnd.component.ts
+++ app/dnd5eapp/dnd.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
 		<nav class="navbar navbar-default">
 			<div class="navbar-header">
 				<a class="navbar-brand" routerLink="/encounter" routerLinkActive="active">Random Encounter Generator </a>
+				
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="true" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 			</div>
 			<div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
 				<ul class="nav navbar-nav">
